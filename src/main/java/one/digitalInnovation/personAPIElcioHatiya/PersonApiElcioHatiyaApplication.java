8fd72be899416aba0a4f6847b0374ca3db1bede5
elcio <elciohatiya@outlook.com>
2021-10-26 12:03:45 -0300

    Carregando repositorio

---
+++ src/main/java/one/digitalInnovation/personAPIElcioHatiya/PersonApiElcioHatiyaApplication.java
@@ -0,0 +1,23 @@
+package one.digitalInnovation.personAPIElcioHatiya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+
+@RestController
+@RequestMapping("/api/v1/people")
+public class PersonApiElcioHatiyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonApiElcioHatiyaApplication.class, args);
+	}
+	@GetMapping
+	public String getBook() {
+
+		return "API Test!";
+	}
+}
